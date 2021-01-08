2cd0f0c74505b5ae262892c1bd00c451348e73b2
Weslei <wesleimsr@gmail.com>
2021-01-08 10:59:50 -0300

    Add install dependencies command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ if [[ -z "${HEX_API_KEY}" ]]; then
 	echo "HEX_API_KEY is not set" && exit 1
 fi
 
+echo "===> install dependencies"
+echo 
+mix do deps.get, deps.compile
+
 echo "===> install hex"
 echo 
 mix local.hex --force
