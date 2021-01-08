63e9210ee4f9c06b1d0a203c587bcb1d0a73f414
Weslei <wesleimsr@gmail.com>
2021-01-08 11:02:47 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ if [[ -z "${HEX_API_KEY}" ]]; then
 	echo "HEX_API_KEY is not set" && exit 1
 fi
 
-echo "===> install dependencies"
-echo 
-mix do deps.get, deps.compile
-
 echo "===> install hex"
 echo 
 mix local.hex --force
 mix local.rebar --force
+
+echo "===> install dependencies"
+echo 
+mix do deps.get, deps.compile
 
 echo "===> Build hex package"
 echo 
